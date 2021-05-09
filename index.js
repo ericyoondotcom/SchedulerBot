d737fa5301ea01532049d7b864e80f68e373a1a9
Eric <emjyoon@gmail.com>
2021-05-09 01:31:57 -0700

    Write lots

---
+++ index.js
@@ -0,0 +1,209 @@
+const Discord = require("discord.js");
+const chrono = require("chrono-node");
+const moment = require("moment");
+const schedule = require("node-schedule");
+
+const TOKEN = "ODQwODMwOTA2MzM0ODM4ODI1.YJd6zQ.FFxyUpxUIFGvod8OTcuge5Z2MfU";
+const PREFIX = "game ";
+const REACTION_EMOJI = "🙋";
+
+let scheduledEvents = [];
+
+const bot = new Discord.Client();
+bot.login(TOKEN);
+
+bot.on("ready", async () => {
+    console.log("Bot running!");
+});
+
+bot.on("message", async (msg) => {
+    if(!msg.cleanContent.startsWith("game ")) return;
+    if(msg.channel.guild == null) return;
+    const cmd = parseCommand(msg.cleanContent.substring(PREFIX.length).trim());
+    console.log(cmd);
+    executeCommand(cmd, msg);
+});
+
+bot.on("messageReactionAdd", async (reaction, user) => {
+    if(user.bot) return;
+    if(reaction.emoji.name != REACTION_EMOJI) return;
+    const event = scheduledEvents.find(i => reaction.message.id == i.msgId);
+    if(event == undefined) return;
+    const member = reaction.message.guild.member(user);
+    if(member == null) return;
+
+    if(event.signups.length >= event.maxPlayers && event.maxPlayers != 0){
+        reaction.users.remove(user);
+        const embed = new Discord.MessageEmbed()
+            .setColor("#ff4f4f")
+            .setTitle("Could not sign up")
+            .setDescription(`The event ${event.name} is already at a maximum of ${event.maxPlayers} signups.`);
+        user.send({embed: embed});
+        return;
+    }
+
+    event.signups.push(user.id);
+    const role = await reaction.message.guild.roles.fetch(event.role);
+    member.roles.add(role);
+
+    const embed = new Discord.MessageEmbed()
+        .setColor("#9cffc0")
+        .setTitle(`Signed up for event ${event.name}!`)
+        .setDescription(`We'll notify you when the event starts on ${moment(event.time).format("ddd, MMM D [at] h:mma")}.`);
+    user.send({embed: embed});
+    return;
+});
+
+bot.on("messageReactionRemove", async (reaction, user) => {
+    if(user.bot) return;
+    if(reaction.emoji.name != REACTION_EMOJI) return;
+    const event = scheduledEvents.find(i => reaction.message.id == i.msgId);
+    if(event == undefined) return;
+    const member = reaction.message.guild.member(user);
+    if(member == null) return;
+    const i = event.signups.indexOf(user.id);
+    if(i < 0) return;
+    event.signups.splice(i, 1);
+    const role = await reaction.message.guild.roles.fetch(event.role);
+    member.roles.remove(role);
+
+    const embed = new Discord.MessageEmbed()
+        .setColor("#ffff9c")
+        .setTitle(`Unregistered for ${event.name}!`)
+        .setDescription(`You've been removed from the event.`);
+    user.send({embed: embed});
+});
+
+bot.on("messageDelete", async (message) => {
+    const eventIdx = scheduledEvents.findIndex(i => message.id == i.msgId);
+    const event = scheduledEvents[eventIdx];
+    if(event == undefined) return;
+    // TODO: cancel cron
+    const role = await message.guild.roles.fetch(event.role);
+    role.delete();
+
+    const embed = new Discord.MessageEmbed()
+        .setColor("#ff4f4f")
+        .setTitle("Canceled: " + event.name)
+        .setDescription("The event previously scheduled for " + moment(event.time).format("ddd, MMM D [at] h:mma") + " has been canceled.");
+    message.channel.send({embed: embed});
+    event.signups.forEach((userId) => {
+        message.guild.members.fetch(userId).then(member => {
+            member.send({embed: embed});
+        });
+    });
+    scheduledEvents.splice(eventIdx, 1);
+})
+
+function parseCommand(text){
+    const splitBySpaces = text.split(" ");
+    if(splitBySpaces.length == 0) return null;
+    const command = splitBySpaces[0];
+    let argsString = text.substring(command.length).trim();
+    const args = {};
+    while(argsString.length > 0){
+        let delimiterIdx = argsString.indexOf(" ");
+        let dashIdx = argsString.indexOf("--");
+        if(dashIdx == -1) break;
+        const arg = argsString.substring(dashIdx + 2, delimiterIdx == -1 ? argsString.length : delimiterIdx);
+        let val;
+        if(delimiterIdx == -1){
+            val = "";
+            argsString = "";
+        }else{
+            const tempValString = argsString.substring(delimiterIdx + 1);
+            const nextDashIdx = tempValString.indexOf("--");
+            val = tempValString.substring(0, nextDashIdx < 0 ? tempValString.length : nextDashIdx);
+            argsString = argsString.substring(delimiterIdx + 1 + val.length);
+        }
+        args[arg] = val.trim();
+    }
+
+    return {command, args};
+}
+
+async function executeCommand(cmd, msg){
+    switch(cmd.command){
+        case "create": { // game create --name Event Name --time Tomorrow at 5pm --minplayers 3 --maxplayers 5
+            if(!("name" in cmd.args)){
+                msg.channel.send("🤡 Missing argument: --name");
+                return;
+            }
+            if(!("time" in cmd.args)){
+                msg.channel.send("🤡 Missing argument: --time");
+                return;
+            }
+            const parsedTime = chrono.parseDate(cmd.args.time);
+            if(parsedTime == null){
+                msg.channel.send("🤡 Invalid start time. Use natural language, e.g. \"Tomorrow at 5:30pm\"");
+                return;
+            }
+            let minPlayers = 0;
+            if("minplayers" in cmd.args){
+                minPlayers = parseInt(cmd.args.minplayers, 10);
+                if(isNaN(minPlayers)) minPlayers = 0;
+            }
+            let maxPlayers = 0;
+            if("maxplayers" in cmd.args){
+                maxPlayers = parseInt(cmd.args.maxplayers, 10);
+                if(isNaN(maxPlayers)) maxPlayers = 0;
+            }
+            if(minPlayers > maxPlayers && maxPlayers != 0){
+                msg.channel.send("😔 Bestie, max players can't be greater than min players!");
+                return;
+            }
+            const embed = new Discord.MessageEmbed()
+                .setColor("#9cffc0")
+                .setTitle(cmd.args.name)
+                .setDescription("React with " + REACTION_EMOJI + " to sign up!")
+                .addField("Starts at", moment(parsedTime).format("ddd, MMM D [at] h:mma"))
+                .setFooter(`Created by ${msg.author.username} | Delete this message to cancel`, msg.author.displayAvatarURL());
+            const mentionsArray = msg.mentions.roles.array();
+            let mentionsString = "";
+            if(mentionsArray.length > 0){
+                for(let i of mentionsArray){
+                    mentionsString += `<@&${i.id}>`;
+                }
+            }
+            if(minPlayers > 0){
+                embed.addField("Min. players to start", minPlayers, true);
+            }
+            if(maxPlayers > 0){
+                embed.addField("Max players", maxPlayers, true);
+            }
+            const newRole = await msg.guild.roles.create({
+                data: {
+                    name: "Signups: " + cmd.args.name,
+                    mentionable: true
+                }
+            });
+            const job = schedule.scheduleJob(parsedTime, () => {
+                onEventStart(sentMessage.id, msg.channel);
+            });
+            const sentMessage = await msg.channel.send({embed: embed, content: mentionsString});
+            sentMessage.react(REACTION_EMOJI);
+            scheduledEvents.push({
+                name: cmd.args.name,
+                time: parsedTime,
+                minPlayers,
+                maxPlayers,
+                msgId: sentMessage.id,
+                signups: [],
+                role: newRole.id,
+                scheduleJob: job
+            });
+            msg.delete();
+            break;
+        }
+        default: {
+            msg.channel.send("🤡 Bestie, that's not a valid command!");
+            return;
+        }
+    }
+}
+
+async function onEventStart(msgId, channel){
+    const event = scheduledEvents.find(i => reaction.message.id == i.msgId);
+    if(event == undefined) return;
+    // TODO
+}
